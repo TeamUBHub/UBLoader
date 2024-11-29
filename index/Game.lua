4db1d3a197b850a156f834539396d208b931c026
R H <165597973+rhuda21@users.noreply.github.com>
2024-11-29 17:39:23 +0000

    Create Game.lua

---
+++ index/Game.lua
@@ -0,0 +1,4 @@
+getgenv().Games = {
+  [15869837051] = "https://raw.githubusercontent.com/UnboundedScripts/balltest_v2/refs/heads/main/script.lua", --Ball Test
+  [82584166303511] = "https://raw.githubusercontent.com/UnboundedScripts/balltest_v2/refs/heads/main/script.lua", --Jimmy the Normal Ball
+}
