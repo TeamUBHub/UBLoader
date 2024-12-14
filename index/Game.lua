30f63e048140e1981ba705b2cdc768b8593862e7
R H <165597973+rhuda21@users.noreply.github.com>
2024-12-14 18:48:08 +0000

    Update Game.lua

--- index/Game.lua
+++ index/Game.lua
@@ -1,6 +1,6 @@
 getgenv().Games = {
-  [15869837051] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/balltest_v2.lua", --Ball Test
-  [82584166303511] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/balltest_v2.lua", --Jimmy the Normal Ball
-  [3840352284] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/Volleyball%204.2.lua", --Volleyball 4.2
-  [11276071411] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/Be%20NPC%20or%20Die.lua", --Be NPC or Die
+  [15869837051] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Ball Test
+  [82584166303511] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Jimmy the Normal Ball
+  [3840352284] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Volleyball%204.2.lua", --Volleyball 4.2
+  [11276071411] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Be%20NPC%20or%20Die.lua", --Be NPC or Die
 }
