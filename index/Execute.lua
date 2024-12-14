b19a3ad4b4be03be3c46995ee4512fe919837b31
R H <165597973+rhuda21@users.noreply.github.com>
2024-12-14 08:56:20 +0000

    Update Execute.lua

--- index/Execute.lua
+++ index/Execute.lua
@@ -1,6 +1,10 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UnboundedScripts/UBLoader/refs/heads/main/index/Game.lua"))()
+
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
+        return -- Exit the loop once the matching script is executed
     end
 end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UnboundedScripts/UniversalUltra/refs/heads/main/script.lua"))()
