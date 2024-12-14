6cd84973defce85b6f345aa7c01861f96f056832
R H <165597973+rhuda21@users.noreply.github.com>
2024-12-14 08:54:04 +0000

    Update Game.lua

--- index/Game.lua
+++ index/Game.lua
@@ -1,5 +1,6 @@
 getgenv().Games = {
   [15869837051] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/balltest_v2.lua", --Ball Test
   [82584166303511] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/balltest_v2.lua", --Jimmy the Normal Ball
-  [3840352284] = "https://raw.githubusercontent.com/UnboundedScripts/Volleyball4.2/refs/heads/main/NextGen.lua", --Volleyball 4.2
+  [3840352284] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/Volleyball%204.2.lua", --Volleyball 4.2
+  [11276071411] = "https://raw.githubusercontent.com/UnboundedScripts/Game-Scripts/refs/heads/main/Be%20NPC%20or%20Die.lua", --Be NPC or Die
 }
