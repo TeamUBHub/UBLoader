b713b9b8cc9e53773a292b120b6812b001422a2b
R H <165597973+rhuda21@users.noreply.github.com>
2024-12-15 12:27:44 +0000

    Update Execute.lua

--- index/Execute.lua
+++ index/Execute.lua
@@ -3,6 +3,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/re
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Key.lua"))()
         return
     end
 end
