3c66dbcafce8f51e1fd15c428ba7116a5593ed25
R H <165597973+rhuda21@users.noreply.github.com>
2024-12-15 12:35:55 +0000

    Update Execute.lua

--- index/Execute.lua
+++ index/Execute.lua
@@ -1,11 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Game.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Key.lua"))()
-
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-        return
-    end
-end
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/UniversalScript.lua"))()
