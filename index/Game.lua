be38a8fec145fedd1002e0c8eb025df1879e138e
R H <165597973+rhuda21@users.noreply.github.com>
2025-01-11 12:12:58 +0000

    Update Game.lua

--- index/Game.lua
+++ index/Game.lua
@@ -1,4 +1,3 @@
-
 getgenv().Games = {
   [15869837051] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Ball Test
   [82584166303511] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Jimmy the Normal Ball
@@ -6,6 +5,4 @@ getgenv().Games = {
   [11276071411] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Be%20NPC%20or%20Die.lua", --Be NPC or Die
   [3678761576] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Entrenched.lua", --ENTRENCHED WW1 BETA
   [12338455185] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/TRT2.lua", -- Tropical Resort tycoon 2
-  [7606564092] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/ShrimpGame.lua", --Shrimp Game
-  [7606602544] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/ShrimpGame.lua", --Shrimp Game (PLAY)
 }
