e4bcebb0536ae207a2ebd8d02d95eaa831d88832
R H <165597973+rhuda21@users.noreply.github.com>
2025-01-30 19:56:15 +0000

    Update Execute.lua

--- index/Execute.lua
+++ index/Execute.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Key.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/EmergencyKey.lua"))()
