00909cc2af4cb635a802840227d65cd0589993bd
R H <165597973+rhuda21@users.noreply.github.com>
2025-03-29 18:05:02 +0000

    Update Game.lua

--- index/Game.lua
+++ index/Game.lua
@@ -1,16 +1,21 @@
 getgenv().Games = {
-  [15869837051] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Ball Test
-  [82584166303511] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Jimmy the Normal Ball
-  --[3840352284] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Volleyball%204.2.lua", --Volleyball 4.2
-  [11276071411] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/BeNPCorDieV2.lua", --Be NPC or Die
-  [3678761576] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Entrenched.lua", --ENTRENCHED WW1 BETA
-  [12338455185] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/TRT2.lua", -- Tropical Resort tycoon 2
-  [7606564092] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/ShrimpGame.lua", --Shrimp Game
-  [7606602544] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/ShrimpGame.lua", --Shrimp Game (PLAY)
-  [10449761463] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/TSB.lua", --The Strongest battlegrounds
-  [16732694052] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Fishc.lua", --Fishc
-  [73956553001240] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Haikyuu.lua", --Vollyball Legends
-  [70876832253163] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/DeadRails.lua", --Dead Rails (GAME)
-  [116495829188952] = "https://gitlab.com/r_soft/main/-/raw/main/Others/deadrails.lua", --Dead Rails [APLHA]
-  [18901165922] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Pet%20GO.lua", -- Pets GO
-}
+    [15869837051] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Ball Test
+    [82584166303511] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/balltest_v2.lua", --Jimmy the Normal Ball
+    --[3840352284] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Volleyball%204.2.lua", --Volleyball 4.2
+    [11276071411] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/BeNPCorDieV2.lua", --Be NPC or Die
+    [3678761576] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Entrenched.lua", --ENTRENCHED WW1 BETA
+    [12338455185] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/TRT2.lua", -- Tropical Resort tycoon 2
+    [7606564092] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/ShrimpGame.lua", --Shrimp Game
+    [7606602544] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/ShrimpGame.lua", --Shrimp Game (PLAY)
+    [10449761463] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/TSB.lua", --The Strongest battlegrounds
+    [16732694052] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Fishc.lua", --Fishc
+    [73956553001240] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Haikyuu.lua", --Vollyball Legends
+    [70876832253163] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/DeadRails.lua", --Dead Rails (GAME)
+    [116495829188952] = "https://gitlab.com/r_soft/main/-/raw/main/Others/deadrails.lua", --Dead Rails [APLHA]
+    [18901165922] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/Pet%20GO.lua", -- Pets GO
+    [2474168535] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/WestBound.lua", -- WestBound
+    [11157166793] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/AntWar.lua", -- Ant War
+    [3101667897] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/LegendsofSpeed.lua", --Legends of speed
+    [14259168147] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/BasketballLegends.lua", --Basketball Legends
+    [142823291] = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/MurderMystery2.lua", --Murder Mystery 2
+  }
