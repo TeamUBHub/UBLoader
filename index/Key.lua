a4f6a84fc5818064fb3111dd40d95244eded0ff2
R H <165597973+rhuda21@users.noreply.github.com>
2025-11-08 20:11:07 +0000

    Update Key.lua

--- index/Key.lua
+++ index/Key.lua
@@ -1,4 +1,4 @@
 getgenv().keyEntered = true
-local s= loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Games.lua"))()[game.PlaceId] or "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/UniversalScript.lua"
+local s= loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Games.lua"))()[game.GameId] or "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/UniversalScript.lua"
 loadstring(game:HttpGet(s))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/JoinUI.lua"))()
